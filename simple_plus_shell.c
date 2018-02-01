c84bb456fef0f17a12ef307dee2df504f32d2074
miaogen <miaogen156@outlook.com>
2018-02-01 19:59:10 +0800

    add sim_pcb

--- simple_plus_shell.c
+++ simple_plus_shell.c
@@ -5,6 +5,9 @@
 #include<sys/wait.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+
+
 
 //the max chars a cmd can have.
 #define MAXCHAR 40
@@ -12,10 +15,9 @@
 #define MAXARGS  20
 
 //DEBUG MODE
-//#define  DEBUG
+#define  DEBUG
 
-//pass to the execve
-char **g_environ;
+
 
 //the proc info struct
 typedef struct sim_pcb{
@@ -33,6 +35,16 @@ typedef struct sim_pcb{
 	struct sim_pcb *next;
 }Pcb;
 
+//pass to the execve
+char **g_environ;
+Pcb *g_pcb;
+
+//signal handler
+typedef void (*sighandler)(int);
+
+#ifdef DEBUG
+void test_handler(int signum);
+#endif
 
 //attention: here return a void * but when using should convert it to a Pcb *
 inline void * Assign_to_Simpcb( pid_t pid, pid_t ppid, unsigned short stat, unsigned short \
@@ -58,7 +70,10 @@ int main(int argc, char *argv[], char *envp[])
 	pcb->running=1;
 	strncpy(pcb->name, "bash", 5);
 	pcb->next=NULL;
+	g_pcb=pcb;
 	
+	if(signal(SIGTSTP,test_handler)==SIG_ERR)
+		unix_error("signal error");
 
 	while(1){
 		printf("> ");
@@ -67,7 +82,6 @@ int main(int argc, char *argv[], char *envp[])
 			exit(0);
 		eval(cmdline, pcb);
 		printf("\n");
-
 	}
 }
 
@@ -86,8 +100,8 @@ void eval (char *cmdline, struct sim_pcb* pcb)
 	char **temp_envir=g_environ;
 	printf("\n");
 	printf("test g_environ\n");
-	while(*temp_envir!=NULL)
-		printf("the temp_envir:%s\n", *temp_envir++);
+//	while(*temp_envir!=NULL)
+//		printf("the temp_envir:%s\n", *temp_envir++);
 	printf("end\n");
 	printf("\n");
 #endif
@@ -106,20 +120,25 @@ void eval (char *cmdline, struct sim_pcb* pcb)
 		}
 		else if(pid>0){
 			//add the proc into the linklist pcb
-			Pcb *new_pcb=Assign_to_Simpcb(pid, getpid(),0, 0, argv[0]);
+			Pcb *new_pcb=(Pcb*)Assign_to_Simpcb(pid, getpid(),0, 0, argv[0]);
 			new_pcb->next=pcb->next;
 			pcb->next=new_pcb;
 			if(!bg){
+				pcb->next->stat=1;
 				int status;
 				if(waitpid(pid, &status, 0)<0)
 					unix_error("waitfg:waitpid error\n");
 				else
 				{
-
+					Pcb *temp_pcb=pcb->next;
+					pcb->next=pcb->next->next;
+					free(temp_pcb);
 				}
 			}
-			else
+			else{
+				pcb->next->stat=1;
 				printf("%d %s", pid, cmdline);
+			}
 
 		}
 	}
@@ -133,7 +152,7 @@ int builtin_command(char **argv)
 		return 1;
 	if(!strcmp(argv[0], "jobs"))
 	{
-		View_Jobs();
+		//View_Jobs();
 		return 1;
 	}
 	if(!strcmp(argv[0], "bg"))
@@ -184,4 +203,15 @@ inline void * Assign_to_Simpcb( pid_t pid, pid_t ppid, unsigned short stat, unsi
 	pcb->next=NULL;
 	return (void*)pcb;
 	
+}
+
+void test_handler(int signum)
+{
+	Pcb* temp_pcb=g_pcb->next;
+	while(temp_pcb!=NULL){
+		printf("pid=%d, ppid=%d, name=%s, stat=%d, running=%d\n", temp_pcb->pid, temp_pcb->ppid, temp_pcb->name \ 
+				,temp_pcb->stat, temp_pcb->running);
+			temp_pcb=temp_pcb->next;
+	}
+
 }
